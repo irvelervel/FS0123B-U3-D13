4ed4f5388a73d09fe8a51e9cf592d5ca64063388
Stefano Casasola <ste.casasola@gmail.com>
2023-04-12 18:22:04 +0700

    Add files via upload

--- index.ts
+++ index.ts
@@ -131,3 +131,101 @@ let epicodeStaffMember = {
 }
 
 console.log(epicodeStaffMember.module.length)
+epicodeStaffMember.firstName = 'Dario'
+
+// INTERFACES
+// un'interfaccia è una struttura dati che definisce le proprietà di un oggetto
+
+interface EpicodeTeacher {
+  firstName: string // obbligatoria
+  lastName: string // obbligatoria
+  module?: string | string[] // facoltativa, una "proprietà opzionale"
+}
+
+let epicodeTeacher1: EpicodeTeacher = {
+  firstName: 'Stefano',
+  lastName: 'Casasola',
+  // module: 'U3',
+}
+
+let epicodeTeacher2: EpicodeTeacher = {
+  firstName: 'Lidia',
+  lastName: 'Kovac',
+  module: ['U2', 'U3'],
+}
+
+let arrayOfTeachers: Array<EpicodeTeacher> = []
+arrayOfTeachers.push(epicodeTeacher1)
+arrayOfTeachers.push(epicodeTeacher2)
+// arrayOfTeachers.push(100) // <-- non funziona, non è un oggetto EpicodeTeacher
+
+arrayOfTeachers.forEach((teacher) => {
+  console.log(teacher.lastName)
+})
+
+interface HumanBeing {
+  numberOfHands: number
+  height: number
+  hairColor: string
+  moustaches: boolean
+}
+
+let marioBros: HumanBeing = {
+  numberOfHands: 2,
+  height: 160,
+  hairColor: 'Brown',
+  moustaches: true,
+}
+
+interface SoccerPlayer extends HumanBeing {
+  team: string
+  favouriteFoot: string
+}
+
+let player: SoccerPlayer = {
+  numberOfHands: 2,
+  height: 175,
+  hairColor: 'blonde',
+  moustaches: false,
+  team: 'Milan',
+  favouriteFoot: 'left',
+}
+
+// GENERICS
+// Un GENERIC è un TIPO passato come un ARGOMENTO ad un'interfaccia
+// nella definizione di interfaccia lo si definisce e lo si assegna come tipo
+// della proprietà designate (perchè non volete che una proprietà abbia sempre
+// lo stesso tipo per ogni utilizzo), e all'utilizzo dell'interfaccia va passato
+// attraverso <>
+
+interface GenericAddress<A> {
+  street: string
+  civicNumber: number
+  zipCode: string
+  city: string
+  area: A
+}
+
+let italianAddress: GenericAddress<string> = {
+  street: 'Via del Campanile',
+  civicNumber: 24,
+  zipCode: '00321',
+  city: 'Cagliari',
+  area: 'Sardinia',
+}
+
+interface AmericanArea {
+  region: string
+  state: string
+}
+
+let americanAddress: GenericAddress<AmericanArea> = {
+  street: '5th Avenue',
+  civicNumber: 345,
+  zipCode: 'sdkfj',
+  city: 'Seattle',
+  area: {
+    region: 'SW',
+    state: 'NY',
+  },
+}
